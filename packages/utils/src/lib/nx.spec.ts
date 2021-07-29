8f50445655c48186d90bc039371af8bb9d333c9f
Ron Netzer <ron@e-square.io>
2021-07-29 15:20:20 +0300

    fix: package.json versions

--- packages/utils/src/lib/nx.spec.ts
+++ packages/utils/src/lib/nx.spec.ts
@@ -46,18 +46,27 @@ describe('nx', () => {
   });
 
   describe('getMaxDistribution', () => {
-    it('should return an array of strings from a string input', () => {
+    it('should return an object of targets distribution from a string input', () => {
       (getInput as jest.Mock).mockReturnValue(2);
       expect(getMaxDistribution('test')).toEqual({ test: 2 });
+    });
 
+    it('should return an object of targets distribution from a JSON object', () => {
       (getInput as jest.Mock).mockReturnValue('{ "test1": 1, "test2": 2 }');
       expect(getMaxDistribution(['test1', 'test2'])).toEqual({ test1: 1, test2: 2 });
+    });
 
+    it('should return an object of targets distribution from a JSON array', () => {
+      (getInput as jest.Mock).mockReturnValue('[1,2]');
+      expect(getMaxDistribution(['test1', 'test2'])).toEqual({ test1: 1, test2: 2 });
+    });
+
+    it('should return an object of targets distribution from a partial JSON object/array', () => {
       (getInput as jest.Mock).mockReturnValue('{ "test1": 1 }');
       expect(getMaxDistribution(['test1', 'test2'])).toEqual({ test1: 1, test2: 3 });
 
-      (getInput as jest.Mock).mockReturnValue('[1,2]');
-      expect(getMaxDistribution(['test1', 'test2'])).toEqual({ test1: 1, test2: 2 });
+      (getInput as jest.Mock).mockReturnValue('[1]');
+      expect(getMaxDistribution(['test1', 'test2'])).toEqual({ test1: 1, test2: 3 });
     });
   });
 
